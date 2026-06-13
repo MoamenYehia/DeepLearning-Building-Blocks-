9cb042aefe92426591b8c3167e415f2b5522eece
MoamenYehia <MoamenYehia@users.noreply.github.com>
2026-06-14 00:30:04 +0300

    forward pass but with built-in functions

--- forwardpass.py
+++ forwardpass.py
@@ -1,10 +1,10 @@
 import torch 
 
-x= torch.tensor([[1,3,6,8]])
+x= torch.tensor([[1,3,6,8]],dtype=torch.float32)
 
-w= torch.tensor([[1,0.5,0.7,0.3],[1,5,9,3]])
+w= torch.tensor([[1,0.5,0.7,0.3],[1,5,9,3]],dtype=torch.float32)
 
-bias=torch.tensor([0.5,1])
+bias=torch.tensor([0.5,1],dtype=torch.float32)
 
 def forward(x,w,bias):
     forward_pass= torch.matmul(x,w.T)+bias
@@ -12,3 +12,13 @@ def forward(x,w,bias):
 
 output=forward(x,w,bias)
 print(output)
+
+#########################################################################
+
+import torch.nn as nn 
+
+layer= nn.Linear(in_features=4,out_features=2)
+
+output=layer(x)
+print(output)
+print(output.shape)
